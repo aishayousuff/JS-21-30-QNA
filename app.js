4e2888037969665c98c01a8471557cf046325008
Aisha Yousuf <aisha.2073.5a@gmail.com>
2024-08-21 00:17:38 +0300

    no comments

---
+++ app.js
@@ -0,0 +1,220 @@
+// Chapter 21
+
+// Question 1
+// var allLower = allLower.toLowerCase()
+
+// Question 2
+// var x = prompt("Enter x value")
+// var y = x.toLowerCase()
+// var x = (y)
+
+// Question 3
+// var y = prompt("Enter y value")
+// var x = y.toUpperCase()
+// var y = (x)
+ 
+// Question 4
+// var name;
+// var name = prompt("Enter your name : ")
+// var nameLowered = name.toLowerCase()
+
+// Question 5
+// var strng = "this text is in uppercase"
+// var strng = strng.toUpperCase()
+// alert(strng)
+
+// Question 6
+// var cityName = "kaRacHi"
+// var capitalizedCityName = cityName.charAt(0).toUpperCase()
+
+// Chapter 22 - 25
+
+// Question 1
+// var sameWords = "captain"
+// var sameWords = sameWords.slice(1,3)
+
+// Question 2
+// var string = "Hello! How are you?"
+// var stringLength = string.length
+
+// Question 3
+// var animal = "elephant"
+// var seg = animal.slice(2, 6)
+ 
+// Question 4
+// var string1 = "abcdefghijklmnopqrstuvwxyz"
+// var string2 = string1.length
+
+// Question 5
+// var string1 = "example"
+// var string2 = string1.length
+// var string3 = string1.slice(1, string2 - 3)
+
+// Question 6
+// var text = "To be or not to be.";
+// var indx = text.indexOf("be");
+// What is the value of indx?
+// The value of indx is 3.
+
+// Question 7
+// var text = "To be or not to be.";
+// var indx = text.lastIndexOf("be");
+// What is the value of indx?
+// The value of indx is 16.
+
+// Question 8
+// var text = "be who you want to be"
+// var indx = text.lastIndexOf("be")
+
+// Question 9
+// if (indexNum >= 0 && indexNum < text.length) {  
+// }
+ 
+// Question 10
+// var string = "abcde"
+// var abc = string.charAt(2)
+
+// Question 11
+// var text = "what are you doing?"
+// var cha = text.charAt(9)
+
+// Question 12
+// var str = "whats your name"
+// var x = str.charAt(str.length - 1)
+
+// Question 13
+// var input = "my name is aisha"
+// var cha = input.charAt(4)
+
+// Question 14
+// if (text.charAt(2) === 'a') {
+// }
+
+// Question 15
+// var text = "abcdefghijklmnopqrstuvwxyz"
+// var charArray = []
+// for (var i = 0; i < text.length; i++) {
+    // charArray[i] = text.charAt(i)
+// }
+// var reply = "correct"
+// var revisedReply = reply.replace("no", "yes")
+ 
+// Question 16
+// var str1 = "my name is aisha. i am 13 years old."
+// var str2 = str1.replace("1", "one")
+
+// Question 17
+// var y = x.replace(/a/g, "z")
+
+
+// Chapter 26
+
+// Question 1
+// var num = Math.round(9.123)
+
+// Question 2
+// var origNum = 2.0000000000000000000000000000000000001
+// var roundNum = Math.ceil(origNum)
+
+// Question 3
+// var origNum = 2.9999999999999999999999999999999999999
+// var roundNum = Math.floor(origNum)
+ 
+// Question 4
+// var origNum = 1.75555555555555555555
+// var roundedNum = Math.round(origNum)
+
+// Question 5
+// var roundedNum = Math.floor(0.5)
+
+
+// Chapter 27
+
+// Question 1
+// var randomNum = Math.floor(Math.random() * 50) + 1
+
+// Question 2
+// var randomNum = Math.random()
+
+// Question 3
+// var dice = Math.floor(Math.random() * 6) + 1
+ 
+// Question 4
+// var toss = Math.floor(Math.random() * 2)
+// var result = (toss === 0) ? "head" : "tail"
+// if(result === 0){
+    // alert("You got heads")
+// }
+// else{
+    // alert("You got tails")
+// }
+
+
+// Chapter 28 - 29
+
+// Question 1
+// How do you convert a string to an integer in JavaScript?
+// We use the parseInt() function.
+
+// Question 2
+// var str = "123456789"
+// var num = parseInt(str, 10)
+
+// Question 3
+// var str = "1.9999999999999999999999999999999999"
+// var num = parseFloat(str)
+ 
+// Question 4
+// var num = num.toString()
+
+// Question 5
+// var num = 42
+// var num = num.toString()
+
+// Question 6
+// var num = "2.2122232425262728"
+// var floatNum = Number(str)
+
+// Chapter 30
+
+// Question 1
+// var num = 3.1415926535
+// var newNum = num.toFixed(4)
+
+// Question 2
+// var num = 1.23456789
+// num = Number(num.toFixed(2))
+
+// Question 3
+// if (num.toFixed(2).length > 4) {}
+ 
+// Question 4
+// var num = 3.1234567
+// alert(num.toFixed(2))
+
+// Question 5
+// 
+
+// Question 6
+
+
+// Chapter 30
+
+// Question 1
+// 
+
+// Question 2
+// 
+
+// Question 3
+// 
+ 
+// Question 4
+// 
+
+// Question 5
+// 
+
+// Question 6
+
+
